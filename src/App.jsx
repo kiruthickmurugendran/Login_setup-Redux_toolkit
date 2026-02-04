29f30a51d5ee70839cbda8f94f010a306efde5de
Kiruthick Rosan <kiruthick9555@gnail.com>
2026-02-04 22:21:20 +0530

    worked on private route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/home'
 import Settings from './pages/settings'
 import User from './pages/user'
 import Login from './pages/login'
+import Privateroute from './components/Privateroute'
 
 
 const App = () => {
@@ -12,9 +13,11 @@ const App = () => {
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Home/>}></Route>
-                <Route path='/settings' element={<Settings/>}></Route>
                 <Route path='/login' element={<Login/>}></Route>
-                <Route path='/user/kiruthick' element={<User/>}></Route>
+                <Route element={<Privateroute/>}>
+                    <Route path='/settings' element={<Settings/>}></Route>
+                    <Route path='/user/kiruthick' element={<User/>}></Route>
+                </Route>
 
             </Routes>
          </BrowserRouter>
